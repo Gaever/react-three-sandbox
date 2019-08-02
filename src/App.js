66901ef2f4cc88563ee2c732af1256d4d04fd348
Gaever <mcgyveroon@gmail.com>
2019-08-02 09:56:01 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Scene from './components/Scene';
+import DraggableCubes from './components/DraggableCubes';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DraggableCubes />
+    </div>
+  );
+}
+
+export default App;
